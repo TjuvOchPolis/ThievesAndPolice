9f18bdb07705426ab532de9f56fcac8b086ace41
Viktor S <viktorsallstrom@hotmail.com>
2025-04-01 14:03:11 +0200

    Add project files.

---
+++ ThievesAndPolice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ThievesAndPolice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
