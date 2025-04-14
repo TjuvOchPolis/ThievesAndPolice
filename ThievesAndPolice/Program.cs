0899f267f97bac7293d7a057f61eeb0cc8694365
Viktor S <viktorsallstrom@hotmail.com>
2025-04-14 13:43:38 +0200

    Sista uppdatering inför redovisning

--- ThievesAndPolice/Program.cs
+++ ThievesAndPolice/Program.cs
@@ -226,7 +226,7 @@ namespace ThievesAndPolice
 
             //Status
             Console.WriteLine("Status: ");
-            Console.WriteLine($"Tjuvar: {people.Where(p => p is Thief).Count()}");
+            Console.WriteLine($"Tjuvar: {people.Where(p => p is Thief).Count() + "  "}");
             Console.WriteLine($"Poliser: {people.Where(p => p is Police).Count()}");
             Console.WriteLine($"Medborgare: {people.Where(p => p is Citizen).Count()}");
         }
